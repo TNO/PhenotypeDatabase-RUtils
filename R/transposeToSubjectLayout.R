3a90a9a7a2054b025aff2532c9a216db3eb6a955
Robert Ernst <rfernst@gmail.com>
2013-07-08 09:19:22 +0200

    Add license boilerplate to .R files.

--- R/transposeToSubjectLayout.R
+++ R/transposeToSubjectLayout.R
@@ -1,6 +1,20 @@
 # Robert Ernst
 # Transpose transcriptomics files to subject layout
 
+# Copyright 2013 Robert Ernst
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ## Example input file 
 ## Feature  Time  Subject_1 Subject_2
 ## 58812    5w    2.34      3.12
