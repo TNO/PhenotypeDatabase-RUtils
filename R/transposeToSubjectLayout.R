25fa9e5b85433eb9df3a2aabcf655d73674927b6
Jordy <jordy.coolen@gmail.com>
2013-10-07 16:10:45 +0200

    Update transposeToSubjectLayout.R

--- R/transposeToSubjectLayout.R
+++ R/transposeToSubjectLayout.R
@@ -34,9 +34,6 @@ transposeToSubjectLayout <- function
  dataPath ##<< path to excel filename
 ){
   
-  library(RAppArmor)
-  rlimit_cpu(120)
-  
   data = read.xls(paste0(dataPath,filename), method='tab', check.names=FALSE) #read excel file, sheet 1
   dataTransposed = t(data) #tranpose data
   
